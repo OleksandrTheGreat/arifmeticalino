211d99fa38ca30388b9b8f4936c5a761281f5f1e
OleksandrTheGreat <oleksandr.victorovych.kuzmenko@gmail.com>
2017-06-18 11:34:51 +0300

    ref

--- webpack.config.test.js
+++ webpack.config.test.js
@@ -3,7 +3,7 @@ const common = require('./webpack.config.common.js');
 module.exports = {
   entry: common.getEntry(),
   output: common.getOutput(),
-  module: common.getModule({ tsconfig: "tsconfig.prod.json" }),
+  module: common.getModule({ tsconfig: "tsconfig.test.json" }),
   resolve: common.getResolve(),
   devtool: "cheap-module-eval-source-map",
   bail: false,
