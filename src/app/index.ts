e861442d479d3174690f58c1f9951b2e59f3403b
OleksandrTheGreat <oleksandr.victorovych.kuzmenko@gmail.com>
2017-06-25 20:31:54 +0300

    start knockout

--- src/app/index.ts
+++
@@ -1,7 +0,0 @@
-// import { Equation, Operations } from '../domain';
-// import { EquationService } from '../services';
-// import { MainPageResourcesEN, MainPageResourcesUA } from '../i18n';
-
-import { ABus } from 'ABus';
-
-let bus = new ABus();
