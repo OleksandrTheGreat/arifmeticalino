29e381b3e1c5f5e6decc7247b8c5e34b0832dd60
OleksandrTheGreat <oleksandr.victorovych.kuzmenko@gmail.com>
2018-02-05 19:03:21 +0200

    language switch

--- src/app/models/AppModel.ts
+++ src/app/models/AppModel.ts
@@ -6,5 +6,18 @@ export class AppModel {
     settings = new SettingsModel([Operations.Add], 1, 1);
     statistics = new StatisticsModel();
     equations = new EquationsModel();
-    i18n: Ii18n = window.location.href.endsWith('en') ? new i18EN() : new i18UA();
+    i18n: Ii18n = window.location.search.indexOf('lang=en') >=0 ? new i18EN() : new i18UA();
+
+    changeLanguage(lang: string): void {
+
+        switch (lang.toLowerCase()) {
+            case 'en':
+                if (!window.location.href.endsWith('#en'))
+                    window.location.href = window.location.href +  '?lang=en';
+                break;
+            case 'ua':
+                window.location.href = window.location.href.replace('?lang=en', '');
+                break;
+        }
+    }
 };
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -5,85 +5,88 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 var
 
-  tsconfig = "tsconfig.prod.json",
+    tsconfig = "tsconfig.prod.json",
 
-  plugins = [
-    new CopyPlugin({
-      from: common.folders.build + "/app/index.html",
-      to: common.folders.bin + '/index.html'
-    }),
-    // new HtmlWebpackPlugin(),
-    new ExtractTextPlugin({
-      filename: 'styles/index.css',
-      allChunks: true
-    }),
-    new CopyPlugin({
-      from: common.folders.root + "/node_modules/bootstrap/dist/css/bootstrap.min.css",
-      to: common.folders.bin + '/styles/bootstrap.min.css'
-    }),
-    new CopyPlugin({
-      from: common.folders.root + "/node_modules/bootstrap/dist/fonts",
-      to: common.folders.bin + '/fonts'
-    })
-  ],
+    plugins = [
+        new CopyPlugin({
+            from: common.folders.build + "/app/index.html",
+            to: common.folders.bin + '/index.html'
+        }),
+        // new HtmlWebpackPlugin(),
+        new ExtractTextPlugin({
+            filename: 'styles/index.css',
+            allChunks: true
+        }),
+        new CopyPlugin({
+            from: common.folders.root + "/node_modules/bootstrap/dist/css/bootstrap.min.css",
+            to: common.folders.bin + '/styles/bootstrap.min.css'
+        }),
+        new CopyPlugin({
+            from: common.folders.root + "/node_modules/bootstrap/dist/fonts",
+            to: common.folders.bin + '/fonts'
+        }),
+        new CopyPlugin({
+            from: common.folders.build + "/app/img",
+            to: common.folders.bin + '/img'
+        })
+    ],
 
-  rules = [
-    // {
-    //   test: /\.ts$/,
-    //   loader: 'tslint-loader',
-    //   options: {
-    //     configFileName: tsconfig
-    //   }
-    // },
-    {
-      test: /\.html$/,
-      use: "html-loader"
-    },
-    {
-      test: /\.s?css$/,
-      use: ExtractTextPlugin.extract({
-        fallback: "to-string-loader",
-        use: [
-          {
-            loader: "css-loader",
-            options: {
-              sourceMap: true,
-              importLoaders: true
-            }
-          },
-          {
-            loader: "sass-loader",
-            options: {
-              sourceMap: true
-            }
-          }
-        ]
-      })
-    },
-    // {
-    //   test: /\.(gif|png|jpe?g|svg)$/i,
-    //   use: [
-    //     'url-loader?name=/assets/images/[hash].[ext]',
-    //     'image-webpack-loader?{optimizationLevel: 7, interlaced: false, pngquant:{quality: "65-90", speed: 4}, mozjpeg: {quality: 65}}'
-    //   ]
-    // },
-    // {
-    //   test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-    //   use: 'url-loader?limit=10000&mimetype=application/font-woff&name=/assets/fonts/[name].[ext]'
-    // },
-    // {
-    //   test: /\.(ttf|eot|otf|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-    //   use: 'url-loader?name=/assets/fonts/[name].[ext]'
-    // }
-  ];
+    rules = [
+        // {
+        //   test: /\.ts$/,
+        //   loader: 'tslint-loader',
+        //   options: {
+        //     configFileName: tsconfig
+        //   }
+        // },
+        {
+            test: /\.html$/,
+            use: "html-loader"
+        },
+        {
+            test: /\.s?css$/,
+            use: ExtractTextPlugin.extract({
+                fallback: "to-string-loader",
+                use: [{
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: true,
+                            importLoaders: true
+                        }
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
+            })
+        },
+        // {
+        //   test: /\.(gif|png|jpe?g|svg)$/i,
+        //   use: [
+        //     'url-loader?name=/assets/images/[hash].[ext]',
+        //     'image-webpack-loader?{optimizationLevel: 7, interlaced: false, pngquant:{quality: "65-90", speed: 4}, mozjpeg: {quality: 65}}'
+        //   ]
+        // },
+        // {
+        //   test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        //   use: 'url-loader?limit=10000&mimetype=application/font-woff&name=/assets/fonts/[name].[ext]'
+        // },
+        // {
+        //   test: /\.(ttf|eot|otf|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        //   use: 'url-loader?name=/assets/fonts/[name].[ext]'
+        // }
+    ];
 
 module.exports = {
-  entry: common.getEntry(),
-  output: common.getOutput(),
-  module: common.getModule({ tsconfig: tsconfig, rules: rules }),
-  resolve: common.getResolve(),
-  plugins: common.getPlugins({ plugins: plugins }),
-  devtool: "source-map",
-  bail: true,
-  cache: false
+    entry: common.getEntry(),
+    output: common.getOutput(),
+    module: common.getModule({ tsconfig: tsconfig, rules: rules }),
+    resolve: common.getResolve(),
+    plugins: common.getPlugins({ plugins: plugins }),
+    devtool: "source-map",
+    bail: true,
+    cache: false
 };
