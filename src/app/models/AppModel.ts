b17482ffae5d6ea50adbf170875036cb9d84ff28
OleksandrTheGreat <oleksandr.victorovych.kuzmenko@gmail.com>
2018-02-05 18:29:49 +0200

    English switch

--- src/app/models/AppModel.ts
+++ src/app/models/AppModel.ts
@@ -1,10 +1,10 @@
 import { Operations } from '../../domain';
 import { SettingsModel, StatisticsModel, EquationsModel } from "./";
-import { Ii18n, i18UA } from "../i18n";
+import { Ii18n, i18UA, i18EN } from "../i18n";
 
 export class AppModel {
     settings = new SettingsModel([Operations.Add], 1, 1);
     statistics = new StatisticsModel();
     equations = new EquationsModel();
-    i18n: Ii18n = new i18UA();
+    i18n: Ii18n = window.location.href.endsWith('en') ? new i18EN() : new i18UA();
 };
